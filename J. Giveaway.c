272a14f0c616b4d594aeb5ef694d58cc7c9e8022
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-11-29 23:48:40 +0600

    Create J. Giveaway.c
    https://codeforces.com/gym/439018/problem/J

---
+++ J. Giveaway.c
@@ -0,0 +1,18 @@
+/*
+Are you hear the name Team_Robust ? It's a programming contest team from Barisal University, Bangladesh.They have done an excellent job in any recent onsite contest.
+
+But unfortunately, this team will not participate in any new onsite contests. Are you sad to hear that? Me also. 😢
+
+Just print the best team name of Barisal University.
+
+Output
+Print the best team name of Barisal University.
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    printf("Team_Robust");
+    return 0;
+}
