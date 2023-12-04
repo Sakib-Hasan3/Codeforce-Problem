cd168e5773b162ad8e3f0b776845ce0a1330a0b6
Sakib Hasan <144396022+Sakib-Hasan3@users.noreply.github.com>
2023-12-04 18:36:50 +0600

    Create Wrong Subtraction.c
    https://codeforces.com/contest/977/problem/A

---
+++ Wrong Subtraction.c
@@ -0,0 +1,55 @@
+/*
+Little girl Tanya is learning how to decrease a number by one, but she does it wrong with a number consisting of two or more digits. Tanya subtracts one from a number by the following algorithm:
+
+if the last digit of the number is non-zero, she decreases the number by one;
+if the last digit of the number is zero, she divides the number by 10 (i.e. removes the last digit).
+You are given an integer number n
+. Tanya will subtract one from it k
+ times. Your task is to print the result after all k
+ subtractions.
+
+It is guaranteed that the result will be positive integer number.
+
+Input
+The first line of the input contains two integer numbers n
+ and k
+ (2≤n≤109
+, 1≤k≤50
+) — the number from which Tanya will subtract and the number of subtractions correspondingly.
+
+Output
+Print one integer number — the result of the decreasing n
+ by one k
+ times.
+
+It is guaranteed that the result will be positive integer number.
+
+Examples
+inputCopy
+512 4
+outputCopy
+50
+inputCopy
+1000000000 9
+outputCopy
+1
+Note
+The first example corresponds to the following sequence: 512→511→510→51→50
+*/
+
+#include<stdio.h>
+int main()
+{
+    int a,b,i,c;
+    scanf("%d %d",&a,&b);
+    for(i=0;i<b;i++){
+        c=a%10;
+        if(c!=0){
+            a=a-1;
+        }
+        else if(c==0){
+            a=a/10;
+        }
+    }
+    printf("%d",a);
+}
